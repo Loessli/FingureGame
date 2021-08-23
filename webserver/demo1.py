ca5ded9a887285cbffc729470a6e9fd3e1f76029
lealli <1055352664@qq.com>
2021-08-23 18:46:10 +0800

    delete

--- webserver/demo1.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-if __name__ == '__main__':
-    data ={
-        "user": "zhuqi"
-    }
-    headers = {'Content-Type': 'application/json;charset=UTF-8'}
-    response = requests.post(url="http://127.0.0.1:51233/search/", json=data, headers=headers)
--- webserver/lib/config.py
+++
@@ -1,4 +0,0 @@
-
-class Config(object):
-    ip = "127.0.0.1"
-    port = 51233
